6d821f55662af0c8884a60ff76636cb13fc3f068
Mr.Finger <zhentoufei@163.com>
2017-04-25 22:23:35 +0800

    2

--- message/views.py
+++ message/views.py
@@ -3,6 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import UserMessage
+
+
 # Create your views here.
 def getform(request):
-    return render(request,'message_form.html')
+    all_messages = UserMessage.objects.all()
+    for message in all_messages:
+        print message.name
+    return render(request, 'message_form.html')
